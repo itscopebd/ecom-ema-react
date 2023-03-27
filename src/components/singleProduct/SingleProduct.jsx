60b88ba5481e18f725ef8bd6b08b0c51832310ab
itscopebd <itscopebd@gmail.com>
2023-03-27 12:35:10 +0600

    product card styles added

--- src/components/singleProduct/SingleProduct.jsx
+++ src/components/singleProduct/SingleProduct.jsx
@@ -3,8 +3,13 @@ import "./SingleProduct.css";
 const SingleProduct = (props) => {
     const {name,price,seller,ratings,img}=props.singleProduct;
     return (
-        <div>
+        <div className='card'>
             <img src={img} alt="" />
+            <h3 className='product__name'>{name}</h3>
+            <h5 className='product__price'>Price: {price}</h5>
+            <h5 className='seller__name'>Manufucture: {seller}</h5>
+            <p className='product__rating'>Rating: {ratings}</p>
+            <button className='btn__card'>Add To Card</button>
         </div>
     );
 };
